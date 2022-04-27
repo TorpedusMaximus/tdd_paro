07aebce2558a6b8e535ef4be0e4784770bb0bc0d
tabaluga <malkowski.kacper@gmail.com>
2022-04-27 19:04:52 +0200

    working bowling calculator app

--- BowlingGame.h
+++ BowlingGame.h
@@ -1,10 +1,78 @@
 #pragma once
 
-class BowlingGame
-{
+using namespace std;
+
+struct Round {
+    int throw1;
+    int throw2;
+    int bonus = 0;
+    int total = 0;
+};
+
+class BowlingGame {
 public:
-	void roll(int pins) {}
-	int getScore() {}
+    void roll(int pins) {
+        if (flag) {
+            flag = false;
+            round.throw1 = pins;
+            round.total += pins;
+            if (pins == 10) {
+                round.bonus = 2;
+                roll(0);
+            }
+        } else {
+            flag = true;
+            round.throw2 = pins;
+            round.total += pins;
+            if (round.throw1 + round.throw2 == 10 && round.bonus == 0) {
+                round.bonus = 1;
+            }
+
+            if (roundNumber > 0) {
+                if (roundScores[roundNumber - 1].bonus == 2) {
+                    roundScores[roundNumber - 1].total += round.throw1 + round.throw2;
+                    if (round.bonus == 2) {
+                        roundScores[roundNumber - 1].bonus = 1;
+                    } else {
+                        roundScores[roundNumber - 1].bonus = 0;
+                    }
+                }
+                if (roundScores[roundNumber - 1].bonus == 1) {
+                    roundScores[roundNumber - 1].total += round.throw1;
+                    roundScores[roundNumber - 1].bonus = 0;
+                }
+            }
+
+            if (roundNumber > 1) {
+                if (roundScores[roundNumber - 2].bonus == 1) {
+                    roundScores[roundNumber - 2].total += round.throw1;
+                }
+            }
+
+            roundNumber += 1;
+            roundScores.push_back(round);
+            round.total = 0;
+            round.bonus = 0;
+        }
+    }
+
+    int getScore() {
+        int score = 0;
+        int i = 0;
+        for (auto item: roundScores) {
+            i++;
+            if (i > 10) {
+                break;
+            }
+            score += item.total;
+        }
+        return score;
+    }
+
 private:
+    bool flag = true;
+    vector<Round> roundScores;
+    int roundNumber = 0;
+    Round round;
 };
 
--- BowlingTDDProject.cpp
+++ BowlingTDDProject.cpp
@@ -1,16 +1,50 @@
 #include "catch/catch.hpp"
 #include "BowlingGame.h"
 
-class SimpleCalculator {
-public:
-    int add(int a, int b) {
-        return a + b;
+
+TEST_CASE("testZerowychRzutow") {
+    BowlingGame game;
+    for (int i = 0; i < 20; i++) {
+        game.roll(0);
     }
-};
+    REQUIRE(game.getScore() == 0);
+}
 
-TEST_CASE("SimpleCalculator_add_test") {
-    SimpleCalculator testObj;
+TEST_CASE("testJednostkowychRzutow") {
+    BowlingGame game;
+    for (int i = 0; i < 20; i++) {
+        game.roll(1);
+    }
+    REQUIRE(game.getScore() == 20);
+}
 
-    REQUIRE( testObj.add(2, 2) == 4);
-    REQUIRE( testObj.add(2, 4) == 6);
+
+TEST_CASE("testSpare") {
+    BowlingGame game;
+    game.roll(6);
+    game.roll(4);
+    game.roll(4);
+    for (int i = 0; i < 17; i++) {
+        game.roll(0);
+    }
+    REQUIRE(game.getScore() == 18);
+}
+
+TEST_CASE("testStrike") {
+    BowlingGame game;
+    game.roll(10);
+    game.roll(3);
+    game.roll(4);
+    for (int i = 0; i < 16; i++) {
+        game.roll(0);
+    }
+    REQUIRE(game.getScore() == 24);
+}
+
+TEST_CASE("perfekcyjnaGra") {
+    BowlingGame game;
+    for (int i = 0; i < 12; i++) {
+        game.roll(10);
+    }
+    REQUIRE(game.getScore() == 300);
 }
